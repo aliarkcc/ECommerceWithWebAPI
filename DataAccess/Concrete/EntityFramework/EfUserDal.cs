b1e2f49634aabdd075cad9ea854749d713634514
mehmet ali <alimehmet1@windowslive.com>
2022-02-11 11:27:55 +0300

    EfUserDal Class'ı oluşturuldu.

---
+++ DataAccess/Concrete/EntityFramework/EfUserDal.cs
@@ -0,0 +1,10 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.Context;
+using Entities.Concrete.BaseEntities;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfUserDal:EfBaseRepository<User,DatabaseContext>,IUserDal
+    {
+    }
+}
