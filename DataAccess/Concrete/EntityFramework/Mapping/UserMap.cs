2411f3990357f1bc7b292d0493cb02b6af966295
mehmet ali <alimehmet1@windowslive.com>
2022-02-11 11:27:38 +0300

    Mapping Klasörü oluşturuldu ve UserMap class'ı eklenerek map şeması tanımlandı.

---
+++ DataAccess/Concrete/EntityFramework/Mapping/UserMap.cs
@@ -0,0 +1,58 @@
+﻿using Entities.Concrete.BaseEntities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace DataAccess.Concrete.EntityFramework.Mapping
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users",@"dbo");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.UserName)
+                .HasColumnName("UserName")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.FirstName)
+                .HasColumnName("FirstName")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.LastName)
+                .HasColumnName("LastName")
+                .HasMaxLength(50)
+                .IsRequired();
+
+            builder.Property(x => x.Password)
+                .HasColumnName("Password")
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(x => x.Gender)
+                .HasColumnName("Gender")
+                .IsRequired();
+
+            builder.Property(x => x.DateOfBirth)
+                .HasColumnName("DateOfBirth")
+                .IsRequired();
+            builder.Property(x => x.CreatedDate).HasDefaultValue(DateTime.Now);
+
+            builder.HasData(new User
+            {
+                Id = 1,
+                FirstName = "Mehmet Ali",
+                LastName = "Arkaç",
+                Password = "1",
+                DateOfBirth = Convert.ToDateTime("01-04-1999"),
+                CreatedDate = DateTime.Now,
+                Address = "İSTANBUL",
+                Email = "alimehmet1@windowslive.com",
+                CreatedUserId = 1,
+                UserName = "admin"
+            });
+        }
+    }
+}
