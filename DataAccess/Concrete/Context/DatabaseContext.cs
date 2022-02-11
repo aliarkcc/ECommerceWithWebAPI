7ebf71d86d8a3a6ddb92f6e73da8fb49a418050c
mehmet ali <alimehmet1@windowslive.com>
2022-02-11 11:27:05 +0300

    Context yeni adı DatabaseContext yapıldı.

---
+++ DataAccess/Concrete/Context/DatabaseContext.cs
@@ -0,0 +1,29 @@
+﻿using DataAccess.Concrete.EntityFramework.Mapping;
+using Entities.Concrete.BaseEntities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.Context
+{
+    public class DatabaseContext:DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options)
+        {
+
+        }
+        public DatabaseContext()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = MEHMETPC\\SQLEXPRESS; Initial Catalog=ECommerceWithWebAPIdb;Integrated Security=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserMap());
+        }
+
+        public virtual DbSet<User> Users{ get; set; }
+    }
+}
