ea05da6564743ef617c99b62547c006abd61ac9a
mehmet ali <alimehmet1@windowslive.com>
2022-02-11 09:34:40 +0300

    Context oluşturuldu.

---
+++ DataAccess/Concrete/Context/Context.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Context
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options):base(options)
+        {
+
+        }
+        public Context()
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = ");
+        }
+    }
+}
